87eb814f25a321aff8fb60f5f68f189ce0adabf9
Nathanael Saraiva <natham_11@hotmail.com>
2021-10-07 00:20:09 -0300

    wip

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,17 +1,11 @@
 import { container } from 'tsyringe';
-
 import { IHashProvider } from '../domain/IHashProvider';
 import FakeHashProvider from '../infra/fakes/FakeHashProvider';
-
 import { ITokenProvider } from '../domain/ITokenProvider';
 import FakeTokenProvider from '../infra/fakes/FakeTokenProvider';
-
 import { IProductRepository } from '../../modules/products/domain/repositories/IProductRepository';
-
 import { IPaymentRepository } from '../../modules/payments/domain/repositories/IPaymentRepository';
-
 import { ICustomerRepository } from '../../modules/customers/domain/repositories/ICustomerRepository';
-
 import { IUserRepository } from '../../modules/users/domain/repositories/IUserRepository';
 import UserRepository from '../../modules/users/infra/prisma/repositories/UserRepository';
 import ProductRepository from '../../modules/products/infra/prisma/repositories/ProductRepository';
