76cdac10e73a14d37fa2849953d0cce86e020f7a
Nathanael Saraiva <natham_11@hotmail.com>
2021-10-07 00:18:06 -0300

    remove not used imports

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,13 +7,10 @@ import { ITokenProvider } from '../domain/ITokenProvider';
 import FakeTokenProvider from '../infra/fakes/FakeTokenProvider';
 
 import { IProductRepository } from '../../modules/products/domain/repositories/IProductRepository';
-import FakeProductRepository from '../../modules/products/infra/fake/repositories/FakeProductRepository';
 
 import { IPaymentRepository } from '../../modules/payments/domain/repositories/IPaymentRepository';
-import FakePaymentRepository from '../../modules/payments/infra/fake/repositories/FakePaymentRepository';
 
 import { ICustomerRepository } from '../../modules/customers/domain/repositories/ICustomerRepository';
-import FakeCustomerRepository from '../../modules/customers/infra/fake/repositories/FakeCustomerRepository';
 
 import { IUserRepository } from '../../modules/users/domain/repositories/IUserRepository';
 import UserRepository from '../../modules/users/infra/prisma/repositories/UserRepository';
